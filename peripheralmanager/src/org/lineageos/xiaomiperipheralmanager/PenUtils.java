d037207e3795ddcd72a06ec2f3394d042b008f13
Davide Garberi <dade.garberi@gmail.com>
2023-10-29 22:28:42 +0100

    sheng: peripheralmanager: Move xiaomi-pen binary to vendor
     * Set properties consequently
    
    Change-Id: Ifd30044c42ddf18b81cb6a05c0228c6f2946f3d7

--- peripheralmanager/src/org/lineageos/xiaomiperipheralmanager/PenUtils.java
+++ peripheralmanager/src/org/lineageos/xiaomiperipheralmanager/PenUtils.java
@@ -31,12 +31,12 @@ public class PenUtils {
 
     private static void enablePenMode() {
         Log.d(TAG, "enablePenMode: Enable Pen Mode");
-        SystemProperties.set("persist.sys.parts.pen", "18");
+        SystemProperties.set("persist.vendor.parts.pen", "18");
     }
 
     private static void disablePenMode() {
         Log.d(TAG, "disablePenMode: Disable Pen Mode");
-        SystemProperties.set("persist.sys.parts.pen", "0");
+        SystemProperties.set("persist.vendor.parts.pen", "0");
     }
 
     private static void refreshPenMode() {
