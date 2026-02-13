cb86a7e60afee06797d6745382906d0d3d958eed
Toufu <hamm157famm7423@gmail.com>
2026-02-14 00:45:44 +0900

    sheng: Add missing tablet core permission file
    Change-Id: I2d6a0f781426a2975da3c58cd5745c028ba5fd4c

--- device.mk
+++ device.mk
@@ -35,6 +35,10 @@ PRODUCT_PACKAGES += \
     SettingsOverlayShengGL \
     SystemUIOverlaySheng
 
+# Permission
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml
+
 # Peripheral Manager
 PRODUCT_PACKAGES += \
     XiaomiPeripheralManager
